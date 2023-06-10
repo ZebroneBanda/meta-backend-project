afe36ca8a10cc62c1bcbff23f13ab80be8dc1947
Zebrone Banda <zebrone.k@gmail.com>
2023-06-10 21:11:28 +0200

    setting up the bookings API

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Booking, Menu
+
+class MenuSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Menu
+        fields = ['title','price','inventory']
+
+
+class BookingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Booking
+        fields = '__all__'
