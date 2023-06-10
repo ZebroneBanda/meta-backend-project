16c5842566e0849063ff01a30e27ca163ddb7657
Zebrone Banda <zebrone.k@gmail.com>
2023-06-10 13:42:55 +0200

    edited the int fields in Booking & Menu models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField
-    booking_date = models.DateTimeField
+    no_of_guests = models.IntegerField(validators=[MinValueValidator(0)])
+    booking_date = models.DateTimeField(auto_now=True)
 
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
 
